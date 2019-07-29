7815f1fb185c0d24a74b08e8ede963829bd495e6
marvin banton <marvin.banton@boom.camp>
2019-07-29 09:52:07 +0800

    Finished Password Hashing

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,27 +1,43 @@
+const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
+const secret = require('../../secret');
+
+module.exports = {
+    create,
+    list,
+    getById,
+    getProfile
+};
+
+
 function create(req, res) {
     const db = req.app.get('db');
 
     const { email, password } = req.body;
-
-    db.users // heres the new stuff, using massive to actually query the database.
-        .insert({
-            email,
-            password,
-            user_profiles: [
-                // this is what is specifying the object
-                // to insert into the related 'user_profiles' table
+    argon2
+        .hash(password)
+        .then((hash) => {
+            return db.users.insert(
                 {
-                    userId: undefined,
-                    about: null,
-                    thumbnail: null,
+                    email,
+                    password: hash,
+                    user_profiles: [
+                        {
+                            userId: undefined,
+                            about: null,
+                            thumbnail: null
+                        }
+                    ]
                 },
-            ],
-        },
-            {
-                deepInsert: true, // this option here tells massive to create the related object
-            }
-        )
-        .then(user => res.status(201).json(user)) // returns a promise so we need to use .then
+                {
+                    fields: ['id', 'email']
+                }
+            );
+        })
+        .then((user) => {
+            const token = jwt.sign({ userId: user.id }, secret);
+            res.status(201).json({ ...user, token });
+        })
         .catch(err => {
             console.error(err); // if something happens we handle the error as well.
             res.status(500).end();
@@ -67,12 +83,5 @@ function getProfile(req, res) {
             res.status(500).end();
         })
 }
-
-module.exports = {
-    create,
-    list,
-    getById,
-    getProfile
-};
 
 // server/index.js - register the handler
